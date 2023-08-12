157682a9e2ca65a42569b3585a0ed89e89287f96
Mykyta <nick90141@gmail.com>
2023-08-12 14:09:33 +0300

    ЗМІНЕНЕ

--- src/task_5/MyClass.java
+++ src/task_5/MyClass.java
@@ -1,13 +1,13 @@
 package task_5;
 
-public class MyClass <T> {
+public class MyClass<T> {
     private T value;
 
     private MyClass(T value) {
         this.value = value;
     }
 
-    public static <T> MyClass<T> factoryMethod(T value) {
+    public static <T> MyClass<T> factoryMethod(Class<T> clazz, T value) {
         return new MyClass<>(value);
     }
 
@@ -16,10 +16,10 @@ public class MyClass <T> {
     }
 
     public static void main(String[] args) {
-        MyClass<String> myStringInstance = MyClass.factoryMethod("name");
+        MyClass<String> myStringInstance = MyClass.factoryMethod(String.class, "name");
         System.out.println(myStringInstance.getValue());
 
-        MyClass<Integer> myIntegerInstance = MyClass.factoryMethod(1);
+        MyClass<Integer> myIntegerInstance = MyClass.factoryMethod(Integer.class, 1);
         System.out.println(myIntegerInstance.getValue());
     }
 }
